74bf7af2c52ef8317bfeb161bce3b18c73c86ff4
Prince-Affeddzie <paffedzie001@st.ug.edu.gh>
2026-05-27 22:23:22 +0000

    refined

--- Controllers/AdminController.js
+++ Controllers/AdminController.js
@@ -142,7 +142,7 @@ const getSingleUser = async(req,res)=>{
     try{
         const {Id} = req.params
 
-        const user = await UserModel.findById(Id).populate('appliedJobs','title').populate('appliedMiniTasks','title budget')
+        const user = await UserModel.findById(Id).populate().populate('appliedMiniTasks','title budget')
         if(!user){
             return res.status(400).json({message:'User not Found'})
         }
